7a26d42f38de156e34a72733512dcb62d7eba14d
ryo hashimoto <ryo.hashimoto.2525@gmail.com>
2024-01-05 15:25:19 +0900

    improved deployment flow

--- packages/schnorr-validation-module/script/SchnorrValidationModule.s.sol
+++ packages/schnorr-validation-module/script/SchnorrValidationModule.s.sol
@@ -8,8 +8,9 @@ contract SchnorrValidationModuleScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        
-        new SchnorrValidationModule{salt: 0}();
+
+        uint256 salt = vm.envUint("DEPLOYMENT_SALT");
+        new SchnorrValidationModule{salt: bytes32(salt)}();
 
         vm.stopBroadcast();
     }
